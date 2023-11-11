4e80e2d209c01925cfd8b9bd0bb4e8ae2550b9fd
timmoh-king <timothykariuki43@gmail.com>
2023-11-11 12:50:22 +0300

    Task 5. Encrypting passwords

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -12,3 +12,10 @@ def hash_password(password: str) -> bytes:
     """
 
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+        Use bcrypt to validate that password matches the hashed password.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
