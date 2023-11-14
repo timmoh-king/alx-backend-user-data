c7bb485dd682a5aa313251887ca41bd111ed8e21
timmoh-king <timothykariuki43@gmail.com>
2023-11-14 10:09:43 +0300

    1. Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -20,7 +20,7 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
-app.errorhandler(401)
+@app.errorhandler(401)
 def unauthorized_error(error) -> str:
     """Not authorized handler
     """
