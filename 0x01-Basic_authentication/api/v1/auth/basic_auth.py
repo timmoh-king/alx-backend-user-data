11f630b0ac4f7adc5f778a5f2a83d971128ecfec
timmoh-king <timothykariuki43@gmail.com>
2023-11-15 16:29:50 +0300

    Task 11. Basic - Overload current_user - and BOOM!

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,6 +6,7 @@ import base64
 import binascii
 from typing import TypeVar
 from api.v1.auth.auth import Auth
+from api.v1.auth.auth import Auth
 from models.base import Base
 from models.user import User
 
@@ -71,7 +72,7 @@ class BasicAuth(Auth):
         """
             returns the User instance based on his email and password.
         """
-        if type(user_email) and type(user_pwd) == str:
+        if isinstance(user_email, str) and isinstance(user_pwd, str):
             try:
                 users = User.search({'email': user_email})
             except Exception:
@@ -81,3 +82,19 @@ class BasicAuth(Auth):
             if users[0].is_valid_password(user_pwd):
                 return users[0]
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+            overloads Auth and retrieves the User instance for a request:
+        """
+        auth_header = Auth.authorization_header(request)
+        base64_auth_token = self.extract_base64_authorization_header(
+            auth_header)
+        decode_auth_token = self.decode_base64_authorization_header(
+            base64_auth_token)
+        user_credentials = self.extract_user_credentials(
+            decode_auth_token)
+        user_email, user_pwd = user_credentials
+        user_obj = self.user_object_from_credentials(
+            user_email=user_email, user_pwd=user_pwd)
+        return user_obj
