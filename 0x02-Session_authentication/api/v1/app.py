4674cc96bee2db9988af261b04ba0efabd182c47
timmoh-king <timothykariuki43@gmail.com>
2023-11-16 15:27:36 +0300

    Task 1. Empty session

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -10,6 +10,7 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 
 
 app = Flask(__name__)
@@ -23,6 +24,9 @@ if auth_type == 'auth':
 
 if auth_type == 'basic_auth':
     auth = BasicAuth()
+
+if auth_type == 'session_auth':
+    auth = SessionAuth()
 
 
 @app.before_request
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+    Create a class SessionAuth that inherits from Auth
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """class SessionAuth that inherits from Auth"""
+    pass
