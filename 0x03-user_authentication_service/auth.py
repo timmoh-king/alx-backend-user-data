e9217a89a07ac7a820d8ad385fff905c515cb379
timmoh-king <timothykariuki43@gmail.com>
2023-11-17 19:59:27 +0300

    Task 4: Hash Password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+    define a _hash_password method that takes in a password
+    string arguments and returns bytes.
+"""
+
+import bcrypt
+import base64
+from typing import ByteString
+
+
+def _hash_password(password: str) -> bytes:
+    """
+        The returned bytes is a salted hash of the input password
+        hashed with bcrypt.hashpw.
+    """
+    return bcrypt.hashpw(password.encode(), password.gensalt())
