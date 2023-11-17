41c7a798ab4e524a6127dbb80132d5857e20a3ee
timmoh-king <timothykariuki43@gmail.com>
2023-11-17 15:27:23 +0300

    Task 1: Create User

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -35,11 +35,11 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """Return a user object"""
-        try:
-            user = User(email=email, hashed_password=hashed_password)
-            self._session.add(user)
-            self._session.commit()
-        except Exception:
-            self._session.rollback()
-            user = None
+        # try:
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        # except Exception:
+        #     self._session.rollback()
+        #     user = None
         return user
