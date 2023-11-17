bf6247c5ff2c086ef23b27634a1a524680effeae
timmoh-king <timothykariuki43@gmail.com>
2023-11-17 15:17:22 +0300

    Task 1: Create User

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -33,7 +33,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> User:
+    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
         """Return a user object"""
         try:
             new_user = User(email=email, hashed_password=hashed_password)
