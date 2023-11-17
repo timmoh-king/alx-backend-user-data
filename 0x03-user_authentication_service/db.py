c5bf77b6d44d27cc28ac10f51c962603cae3daca
timmoh-king <timothykariuki43@gmail.com>
2023-11-17 19:20:38 +0300

    Task 1: Add User

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -21,7 +21,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -47,22 +47,22 @@ class DB:
             user = None
         return user
 
-    def find_user_by(self, **kwargs) -> User:
-        """
-            returns the first row found in the users table
-            as filtered by the method’s input arguments
-        """
-        columns, values = [], []
-        for key, value in kwargs.items():
-            if hasattr(User, key):
-                columns.append(getattr(User, key))
-                values.append(value)
-            else:
-                raise InvalidRequestError()
+    # def find_user_by(self, **kwargs) -> User:
+    #     """
+    #         returns the first row found in the users table
+    #         as filtered by the method’s input arguments
+    #     """
+    #     columns, values = [], []
+    #     for key, value in kwargs.items():
+    #         if hasattr(User, key):
+    #             columns.append(getattr(User, key))
+    #             values.append(value)
+    #         else:
+    #             raise InvalidRequestError()
 
-        first_record = self._session.query(User).filter(
-            tuple_(*columns).in_([tuple(values)])
-        ).first()
-        if first_record is None:
-            raise NoResultFound()
-        return first_record
+    #     first_record = self._session.query(User).filter(
+    #         tuple_(*columns).in_([tuple(values)])
+    #     ).first()
+    #     if first_record is None:
+    #         raise NoResultFound()
+    #     return first_record
