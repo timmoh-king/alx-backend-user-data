1ede5f2a659551296694066eda83fc0ff0f53aa0
timmoh-king <timothykariuki43@gmail.com>
2023-11-17 21:45:30 +0300

    Task 7. Register User

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -4,18 +4,36 @@
     Create a Flask app that has a single GET route ("/")
     use flask.jsonify to return a JSON payload of the form:
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import (CORS, cross_origin)
-
+from auth import Auth
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index() -> str:
     """return a JSON payload of the form"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """
+        function that implements the POST /users route.
+        The end-point should expect two form data fields:
+        "email" and "password".
+    """
+    email, password = request.form.get("email"), request.form.get("password")
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": "email", "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
