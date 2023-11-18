3ba524cb4d898ada786b337c1db765a5a58d51b7
timmoh-king <timothykariuki43@gmail.com>
2023-11-18 11:14:58 +0300

    Updated Task 15. User profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -67,5 +67,20 @@ def logout() -> str:
     return redirect("/")
 
 
+@app.route("/profile", methods=['GET'], strict_slashes=False)
+def profile() -> str:
+    """
+    Use it to find the user.
+    If the user exist, respond with a 200 HTTP status
+    """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user is not None:
+        return jsonify({{"email": f"{user.email}"}}), 200
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
