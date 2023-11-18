216fa2cdd95cf415b0fe668d351d91428d341280
timmoh-king <timothykariuki43@gmail.com>
2023-11-18 09:51:45 +0300

    Task 13: Destroy session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -91,3 +91,9 @@ class Auth:
                 return None
         else:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """The method updates the corresponding user’s session ID to None"""
+        if user_id is None:
+            return None
+        self._db.update_user(user_id, session_id=Nones)
